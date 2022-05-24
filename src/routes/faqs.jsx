bfa1312969f054613572f7cebe30b9b592ce351b
Laura-Chiquillo <lchiquillo@unbosque.edu.co>
2022-05-23 20:06:39 -0500

    proyectos

--- src/routes/faqs.jsx
+++ src/routes/faqs.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import proyecto from '../imagenes/proyectos.jpg'
 import ProyectsLayout  from '../components/proyectsLayout'
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cube";
 import "swiper/css/pagination";
+import proyecto1 from '../imagenes/proyecto_nucleo2.png'
+import proyecto2 from '../imagenes/software_1.png'
 
 import { EffectCube, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,7 +14,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function FaqsPage() {
   return (
-    <ProyectsLayout title="Te amo amorcito">
+    <ProyectsLayout title="Mis proyectos">
       <Swiper
         effect={"cube"}
         grabCursor={true}
@@ -28,10 +29,10 @@ export default function FaqsPage() {
         className="absolute w-2/4 -bottom-64 lg:top-32 lg:-left-56"
       >
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <img src={proyecto1}  />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          <img src={proyecto2} />
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
